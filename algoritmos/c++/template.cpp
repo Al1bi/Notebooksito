211a0f6c65abcd8b19f9b69c956301f91fbbbcc2
Al1bi <denilson.fiesta@ucb.edu.bo>
2023-08-06 21:09:11 -0400

    adding the template file

---
+++ algoritmos/c++/template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define umap unordered_map
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+
+typedef long long int ll;
+typedef long double ld;
+
+const int INF = 1e9;
+const int MAX = 1e6+7;
+const double PI = acos(-1);
+const double EPS = 1e-9;
+
+int dx[] = {1, 0, -1, 1};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+
+  
+
+}
