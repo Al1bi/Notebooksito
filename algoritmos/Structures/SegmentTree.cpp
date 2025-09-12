f2101a92b21cbcecb26c790cf2aeb1dd6bb605eb
denilsonFiesta <inocentedenil@gmail.com>
2025-09-11 23:03:24 -0400

    Segmente tree refactorizado

--- algoritmos/Structures/SegmentTree.cpp
+++ algoritmos/Structures/SegmentTree.cpp
@@ -1,35 +1,34 @@
-const int N = 1e5;
-int t[4 * N];
-
-int merge(int nodeL, int nodeR){
-  return nodeL + nodeR;
-}
-
-void build(int a[], int v, int tl, int tr){
-  if(tl == tr) t[v] = a[tl];
-  else{
-    int tm = (tl + tr)/2;
-    build(a, v*2, tl , tm);
-    build(a, v*2+1, tm+1, tr);
-    t[v] = merge(t[v*2], t[v*2+1]);
+struct stree{
+  int n, neutro;
+  vector<int> t;
+  stree(int n): n(n), t(4*n+7) {}
+  int merge(int nodeL, int nodeR){
+    return nodeL + nodeR;
   }
-}
-
-int query(int v, int tl, int tr, int l, int r){
-  if(l > r) return 0;
-  if(l == tl && r == tr) return t[v];
-  int tm = (tl + tr)/2;
-  int nodeL = query(v*2, tl, tm, l, min(r, tm) );
-  int nodeR = query(v*2+1, tm+1, tr, max(l, tm+1), r);
-  return merge(nodeL, nodeR);
-}
-
-void update(int v, int tl, int tr, int pos, int new_val){
-  if(tl == tr) t[v] = new_val;
-  else{
-    int tm = (tl + tr)/2;
-    if(pos <= tm) update(v*2, tl, tm, pos, new_val);
-    else update(v*2+1, tm+1, tr, pos, new_val);
-    t[v] = merge(t[v*2], t[v*2+1]);
+  void build(int v, int tl, int tr, vector<int>& a){
+    if(tl == tr) t[v] = a[tl];
+    else{
+      int tm = (tl + tr) / 2;
+      build(2*v, tl, tm, a);
+      build(2*v+1, tm+1, tr, a);
+      t[v] = merge(t[2*v], t[2*v+1]);
+    }
   }
-}
+  int query(int v, int tl, int tr, int l, int r){
+    if(l > r) return neutro;
+    if(l == tl && r == tr) return t[v];
+    int tm = (tl + tr) / 2;
+    int nodeL = query(2*v, tl, tm, l, min(r, tm));
+    int nodeR = query(2*v+1, tm+1, tr, max(tm+1, l), r);
+    return merge(nodeL, nodeR);
+  }
+  void update(int v, int tl, int tr, int pos, int nv){
+    if(tl == tr) t[v] = nv;
+    else{
+      int tm = (tl + tr) / 2;
+      if(pos <= tm) update(2*v, tl, tm, pos, nv);
+      else update(2*v+1, tm+1, tr, pos, nv);
+      t[v] = merge(t[2*v], t[2*v+1]);
+    }
+  }
+};
